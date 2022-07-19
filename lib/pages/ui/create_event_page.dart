1d365c6814aa169e71efad08239bf074ad867a21
JonnyOnPC <jonathanzhaon@gmail.com>
2022-07-18 22:57:42 -0400

    fix: create_event_page
    Renamed from create_invite page, added TextEditingControllers test code.

--- lib/pages/ui/create_event_page.dart
+++ lib/pages/ui/create_event_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:group_lunch_app/shared/user_model.dart';
+import 'package:group_lunch_app/shared/event_model.dart';
 
 class CreateInvitePage extends StatelessWidget {
   @override
@@ -17,9 +18,34 @@ class EnterEventInfo extends StatefulWidget {
 
 class _EnterEventInfoState extends State<EnterEventInfo> {
   final _formKey = GlobalKey<FormState>();
+  final numControllers = 2;
+  //For Event Info
+  List<TextEditingController> inputControllerList = [];
+  // Create text editing controllers
+  void createTextEditingControllerList(
+      int numControllers, List<TextEditingController> controllers) {
+    for (int i = 0; i < numControllers; i++) {
+      TextEditingController controller = TextEditingController();
+      controllers.add(controller);
+    }
+  }
+
+  //For Participant Info
+  List<UserModel> invitees = [];
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    for (TextEditingController controller in inputControllerList) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    //Create the text editing controllers
+    createTextEditingControllerList(numControllers, inputControllerList);
     //User Inputs Information
     return Scaffold(
       body: Stack(
@@ -28,20 +54,22 @@ class _EnterEventInfoState extends State<EnterEventInfo> {
               key: _formKey,
               child: Column(
                 children: <Widget>[
-                  titleTextField('Add Title'),
+                  titleTextField('Add Title', inputControllerList[0]),
                   //Add Text Inbetween these
                   Text('Date'),
-                  titleTextField('Add Date'),
+                  titleTextField('Add Date', inputControllerList[1]),
                   //Add Text Inbetween these
                   Padding(
                     padding: EdgeInsets.all(16.0),
                     child: Text('Add People or Phone Numbers:'),
                   ),
-                  dropdownSearchField('Add People or Phone Numbers'),
+                  dropdownSearchField('Add People or Phone Numbers', invitees),
                   ElevatedButton(
                     onPressed: () {
                       // Enters if statement = form is valid
                       if (_formKey.currentState!.validate()) {
+                        //Create event with inputted information
+
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(content: Text('Sending Invite...')),
                         );
@@ -57,8 +85,10 @@ class _EnterEventInfoState extends State<EnterEventInfo> {
   }
 }
 
-Widget titleTextField(String baseText) {
+//try .map??? to get text, creates a function for each element in list that returns an iterable
+Widget titleTextField(String baseText, TextEditingController inputController) {
   return TextFormField(
+    controller: inputController,
     // The validator receives the text that the user has entered.
     validator: (value) {
       if (value == null || value.isEmpty) {
@@ -73,7 +103,7 @@ Widget titleTextField(String baseText) {
   );
 }
 
-Widget dropdownSearchField(String baseText) {
+Widget dropdownSearchField(String baseText, List<UserModel> invitees) {
   // return DropdownSearch<String>.multiSelection(
   //   items: [
   //     "Jonathan",
@@ -109,6 +139,6 @@ Widget dropdownSearchField(String baseText) {
     items: listOfUsers,
     itemAsString: (UserModel u) => u.userAsString(),
     onChanged: (List<UserModel?> data) => print(data),
-    selectedItems: [],
+    selectedItems: invitees,
   );
 }

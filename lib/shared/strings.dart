4ca417797eed3081ac9948289e82f8d76c3d0767
John Huang <john.jl.huang@gmail.com>
2022-08-07 18:08:15 -0400

    event firestore methods init

--- lib/shared/strings.dart
+++ lib/shared/strings.dart
@@ -1,7 +1,27 @@
-class Strings {
-  static const String USER_ID_KEY = 'user_id';
-  static const String USER_CREATED_AT_KEY = 'user_created_at';
-  static const String USER_NAME_KEY = 'user_name';
-  static const String USER_PHONE_NUMBER_KEY = 'user_phone_number';
-  static const String USER_AVATAR_KEY = 'user_avatar';
-}
+// User Model
+const String USER_AVATAR_KEY = 'user_avatar';
+const String USER_CREATED_AT_KEY = 'created_at';
+const String USER_ID_KEY = 'user_id';
+const String USER_NAME_KEY = 'name';
+const String USER_PHONE_NUMBER_KEY = 'phone_number';
+
+// Event Model
+const String EVENT_ACTIVE_KEY = 'is_active';
+const String EVENT_CREATED_AT_KEY = 'created_at';
+const String EVENT_DESCRIPTION_KEY = 'description';
+const String EVENT_END_TIME_KEY = 'end_time';
+const String EVENT_HOST_ID_KEY = 'host';
+const String EVENT_HOST_DATA_KEY = 'host_data';
+const String EVENT_ID_KEY = 'event_id';
+const String EVENT_NAME_KEY = 'name';
+const String EVENT_PARTICIPANTS_KEY = 'participants';
+const String EVENT_START_TIME_KEY = 'start_time';
+
+const String EVENT_DURATION_TIME_KEY = 'duration';
+const String EVENT_INVITED_USERS_KEY = 'invited_users';
+const String EVENT_ACCEPTED_USERS_KEY = 'accepted_users';
+const String EVENT_DECLINED_USERS_KEY = 'declined_users';
+const String EVENT_TENTATIVE_USERS_KEY = 'tentative_users';
+const String EVENT_DECLINED_KEY = 'declined';
+const String EVENT_ACCEPTED_KEY = 'accepted';
+const String EVENT_TENTATIVE_KEY = 'tentative';
